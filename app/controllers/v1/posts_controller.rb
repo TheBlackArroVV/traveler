6ac1076e0ba3c0a3ab0e10e55778a04c91b93b80
TheBlackArroVV <theblackarrovv@gmail.com>
2018-06-12 18:27:59 +0300

    some more fixes for authentication

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,5 +1,5 @@
 class V1::PostsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, only: [:create, :update, :destroy]
   before_action :set_post, only: [:show, :destroy, :update]
 
   def index
