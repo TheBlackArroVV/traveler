51501437c928378cb5845828674cedcd796f404c
TheBlackArroVV <theblackarrovv@gmail.com>
2018-10-13 18:23:26 +0300

    delete unused

--- app/controllers/api/v1/users/user_token_controller.rb
+++ app/controllers/api/v1/users/user_token_controller.rb
@@ -3,6 +3,7 @@ module Api
     module Users
       class UserTokenController < Knock::AuthTokenController
         skip_before_action :verify_authenticity_token
+
         private
 
         def auth_params
