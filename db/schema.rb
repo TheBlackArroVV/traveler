2e4b08ef38c48cb2c01002040d53a14c2919d3d3
Vasya <theblackarrovv@gmail.com>
2018-11-27 16:23:47 +0200

    create images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_127_094_716) do
+ActiveRecord::Schema.define(version: 20_181_127_142_001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20_181_127_094_716) do
     t.string 'name'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'images', force: :cascade do |t|
+    t.string 'image'
+    t.bigint 'sight_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['sight_id'], name: 'index_images_on_sight_id'
   end
 
   create_table 'posts', force: :cascade do |t|
--- spec/models/sight_spec.rb
+++ spec/models/sight_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Sight, type: :model do
   it { is_expected.to belong_to :city }
+  it { is_expected.to have_many :images }
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :description }
 end
