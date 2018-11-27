47c142c6f08505bfd0296fa0b9d21136846c981d
Vasya <theblackarrovv@gmail.com>
2018-11-27 11:38:08 +0200

    add countries

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_820_102_114) do
+ActiveRecord::Schema.define(version: 20_181_127_093_517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'countries', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'posts', force: :cascade do |t|
     t.string 'title'
