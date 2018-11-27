f6b10b4e4ceafa78cee475b4ad8b6e4a96223391
Vasya <theblackarrovv@gmail.com>
2018-11-27 11:46:40 +0200

    add cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_127_093_517) do
+ActiveRecord::Schema.define(version: 20_181_127_094_003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'cities', force: :cascade do |t|
+    t.string 'name'
+    t.bigint 'country_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['country_id'], name: 'index_cities_on_country_id'
+  end
 
   create_table 'countries', force: :cascade do |t|
     t.string 'name'
