27d4f569978b150d9cbe0c9f33ba5ba705ad6b88
TheBlackArroVV <theblackarrovv@gmail.com>
2019-01-03 17:31:20 +0200

    membership model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_103_151_917) do
+ActiveRecord::Schema.define(version: 20_190_103_152_745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20_190_103_151_917) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['sight_id'], name: 'index_images_on_sight_id'
+  end
+
+  create_table 'memberships', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.bigint 'trip_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['trip_id'], name: 'index_memberships_on_trip_id'
+    t.index ['user_id'], name: 'index_memberships_on_user_id'
   end
 
   create_table 'messages', force: :cascade do |t|
