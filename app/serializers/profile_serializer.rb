d4a6acb2d53b507ebafb6ee7df01d0e5112629fe
TheBlackArroVV <theblackarrovv@gmail.com>
2019-02-28 16:33:02 +0200

    add user.admin to profile_serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,8 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes(*Profile.attribute_names.map(&:to_sym) - %i[created_at updated_at])
+  attribute :admin
+
+  def admin
+    @object.user.admin
+  end
 end
